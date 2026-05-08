5752ea04f4f283e7c2df0fac2bee1a106630d459
jwweer <hvb67cpgkg@privaterelay.appleid.com>
2026-05-08 13:02:39 +0700

    aaa

--- imp/papka21/file5.c
+++ imp/papka21/file5.c
@@ -3,73 +3,60 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define MAX_PIXELS 160005
+#define MAXN 160005
+#define MAXM 2000000
 
 typedef struct Edge {
     int to, rev;
-    int cap, flow;
+    long long cap, flow;
+    struct Edge *next;
 } Edge;
 
-Edge* graph[MAX_PIXELS];
-int graph_size[MAX_PIXELS];
-int graph_cap[MAX_PIXELS];
+Edge *graph[MAXN];
+int level[MAXN], iter[MAXN];
+int h, w, total_nodes, src, sink;
 
-int level[MAX_PIXELS];
-int iter[MAX_PIXELS];
-
-int h, w, total_nodes;
-int src, sink;
-
-void add_edge(int from, int to, int cap) {
-    Edge e1 = {to, graph_size[to], cap, 0};
-    Edge e2 = {from, graph_size[from], 0, 0};
+void add_edge(int from, int to, long long cap) {
+    Edge *e1 = malloc(sizeof(Edge));
+    Edge *e2 = malloc(sizeof(Edge));
     
-    if (graph_size[from] >= graph_cap[from]) {
-        graph_cap[from] = graph_cap[from] == 0 ? 4 : graph_cap[from] * 2;
-        graph[from] = realloc(graph[from], graph_cap[from] * sizeof(Edge));
-    }
-    if (graph_size[to] >= graph_cap[to]) {
-        graph_cap[to] = graph_cap[to] == 0 ? 4 : graph_cap[to] * 2;
-        graph[to] = realloc(graph[to], graph_cap[to] * sizeof(Edge));
-    }
+    e1->to = to; e1->cap = cap; e1->flow = 0; e1->rev = 0; e1->next = graph[from];
+    e2->to = from; e2->cap = 0; e2->flow = 0; e2->rev = 0; e2->next = graph[to];
     
-    graph[from][graph_size[from]++] = e1;
-    graph[to][graph_size[to]++] = e2;
+    graph[from] = e1;
+    graph[to] = e2;
+    
+    e1->rev = 1;
+    e2->rev = 0;
 }
 
 void bfs(int s) {
-    int queue[MAX_PIXELS];
-    int front = 0, rear = 0;
-    
-    for (int i = 0; i < total_nodes; i++) {
-        level[i] = -1;
-    }
-    
+    memset(level, -1, total_nodes * sizeof(int));
+    int q[MAXN], front = 0, rear = 0;
     level[s] = 0;
-    queue[rear++] = s;
+    q[rear++] = s;
     
     while (front < rear) {
-        int v = queue[front++];
-        for (int i = 0; i < graph_size[v]; i++) {
-            Edge *e = &graph[v][i];
+        int v = q[front++];
+        for (Edge *e = graph[v]; e; e = e->next) {
             if (e->cap > e->flow && level[e->to] < 0) {
                 level[e->to] = level[v] + 1;
-                queue[rear++] = e->to;
+                q[rear++] = e->to;
             }
         }
     }
 }
 
-int dfs(int v, int t, int f) {
+long long dfs(int v, int t, long long f) {
     if (v == t) return f;
-    
-    for (; iter[v] < graph_size[v]; iter[v]++) {
-        Edge *e = &graph[v][iter[v]];
+    for (Edge *e = graph[v]; e; e = e->next) {
         if (e->cap > e->flow && level[v] < level[e->to]) {
-            int d = dfs(e->to, t, f < e->cap - e->flow ? f : e->cap - e->flow);
+            long long d = dfs(e->to, t, f < e->cap - e->flow ? f : e->cap - e->flow);
             if (d > 0) {
                 e->flow += d;
-                graph[e->to][e->rev].flow -= d;
+                Edge *rev = graph[e->to];
+                for (int i = 0; i < e->rev; i++) rev = rev->next;
+                rev->flow -= d;
                 return d;
             }
         }
@@ -77,78 +64,71 @@ int dfs(int v, int t, int f) {
     return 0;
 }
 
-int max_flow(int s, int t) {
-    int flow = 0;
+long long max_flow() {
+    long long flow = 0;
     while (1) {
-        bfs(s);
-        if (level[t] < 0) return flow;
-        
-        for (int i = 0; i < total_nodes; i++) {
-            iter[i] = 0;
-        }
-        
-        int f;
-        while ((f = dfs(s, t, INT_MAX)) > 0) {
+        bfs(src);
+        if (level[sink] < 0) return flow;
+        memset(iter, 0, total_nodes * sizeof(int));
+        long long f;
+        while ((f = dfs(src, sink, LLONG_MAX)) > 0) {
             flow += f;
         }
     }
 }
 
 int pixel_id(int r, int c) {
-    return (r - 1) * w + (c - 1) + 2;
+    return (r-1)*w + c + 2;
 }
 
 int main() {
     scanf("%d %d", &h, &w);
-    
     total_nodes = h * w + 2;
-    src = 0;
-    sink = 1;
+    src = 0; sink = 1;
     
-    for (int i = 0; i < total_nodes; i++) {
-        graph[i] = NULL;
-        graph_size[i] = 0;
-        graph_cap[i] = 0;
-    }
+    for (int i = 0; i < total_nodes; i++) graph[i] = NULL;
+    
+    long long sum_b = 0;
     
     for (int r = 1; r <= h; r++) {
         for (int c = 1; c <= w; c++) {
-            int a, b;
-            scanf("%d %d", &a, &b);
+            long long a, b;
+            scanf("%lld %lld", &a, &b);
             int id = pixel_id(r, c);
             add_edge(src, id, a);
             add_edge(id, sink, b);
+            sum_b += b;
         }
     }
     
     int m;
     scanf("%d", &m);
     for (int i = 0; i < m; i++) {
-        int r1, c1, r2, c2, p;
-        scanf("%d %d %d %d %d", &r1, &c1, &r2, &c2, &p);
+        int r1, c1, r2, c2;
+        long long p;
+        scanf("%d %d %d %d %lld", &r1, &c1, &r2, &c2, &p);
         int id1 = pixel_id(r1, c1);
         int id2 = pixel_id(r2, c2);
         add_edge(id1, id2, p);
         add_edge(id2, id1, p);
     }
     
-    int min_cut_value = max_flow(src, sink);
+    long long min_cut = max_flow();
+    long long min_energy = sum_b - min_cut;
     
-    printf("%d\n", min_cut_value);
+    printf("%lld\n", min_energy);
     
-    int reachable[MAX_PIXELS] = {0};
-    int queue[MAX_PIXELS];
-    int front = 0, rear = 0;
-    queue[rear++] = src;
+    int reachable[MAXN] = {0};
+    int q[MAXN], front = 0, rear = 0;
+    q[rear++] = src;
     reachable[src] = 1;
     
     while (front < rear) {
-        int v = queue[front++];
-        for (int i = 0; i < graph_size[v]; i++) {
-            Edge *e = &graph[v][i];
+        int v = q[front++];
+        for (Edge *e = graph[v]; e; e = e->next) {
             if (e->cap > e->flow && !reachable[e->to]) {
                 reachable[e->to] = 1;
-                queue[rear++] = e->to;
+                q[rear++] = e->to;
             }
         }
     }
@@ -159,10 +139,6 @@ int main() {
             printf("%d", reachable[id] ? 0 : 1);
         }
         printf("\n");
-    }
-    
-    for (int i = 0; i < total_nodes; i++) {
-        if (graph[i]) free(graph[i]);
     }
     
     return 0;
