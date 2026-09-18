b6924688c006533c023e0f791db8fdc155318e99
jwweer <hvb67cpgkg@privaterelay.appleid.com>
2026-09-18 23:11:58 +0700

    Исправила дз по ооп и добавила описание

--- ооп/ex1/hypotenuse.h
+++ ооп/ex1/hypotenuse.h
@@ -5,7 +5,7 @@
 
 namespace Hypo {
 
-    template <typename T>
+    template <typename T> // переделали функцию с помощью шаблона для того, чтобы на вход могли подаваться переменные любого типа
     T hypotenuse(const T& a, const T& b) {
         return std::sqrt(a * a + b * b);
     }
@@ -18,5 +18,8 @@ namespace Hypo {
     }
 
 }
+//Изначально у нас была функция в файле hypotenuse.cpp, там две функции, которые принимают разные типы данных изза чего компилятор не ругается
+//namespace сделан для того, чтобы у файлов hypotenuse.cpp и hypotenuse_m.cpp были одинаковые названия у функции
+//обращение к фуекции в namespace выглядит так int h1 = Hypo::hypotenuse(a, b); здесь мы вызываем из namespace Hypo функцию hypotenuse
 
 #endif
--- ооп/ex1/main.cpp
+++ ооп/ex1/main.cpp
@@ -7,25 +7,19 @@ int main() {
     int a, b;
     std::cin >> a >> b;
 
-    int h1 = Hypo::hypotenuse(a, b);
-    int h2 = Hypom::hypotenuse(a, b);
-
-    std::cout << "Hypo  int: " << h1 << std::endl;
-    std::cout << "Hypom int: " << h2 << std::endl;
+    std::cout <<  Hypo::hypotenuse(a, b) << std::endl;
+    std::cout << Hypom::hypotenuse(a, b) << std::endl;
 
     double da, db;
     std::cin >> da >> db;
 
-    double dh1 = Hypo::hypotenuse(da, db);
-    double dh2 = Hypom::hypotenuse(da, db);
-
-    std::cout << "Hypo  double: " << dh1 << std::endl;
-    std::cout << "Hypom double: " << dh2 << std::endl;
+    std::cout << Hypo::hypotenuse(da, db) << std::endl;
+    std::cout << Hypom::hypotenuse(da, db) << std::endl;
 
     int x = 3, y = 4;
-    std::cout << "Before modify: x = " << x << ", y = " << y << std::endl;
+    std::cout << "Before: x = " << x << ", y = " << y << std::endl;
     Hypo::hypotenuse_modify(x, y);
-    std::cout << "After  modify: x = " << x << ", y = " << y << std::endl;
+    std::cout << "After: x = " << x << ", y = " << y << std::endl;
 
     return 0;
 }
